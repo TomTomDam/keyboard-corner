886c62fb8336d720f275493d6e8f3ad10235d02f
TomTomDam <thomasdam97@gmail.com>
2021-01-12 16:25:32 +0000

    Changed Keyboard classes to functions, since there is no need to change state as far as I can see

--- src/components/MyKeyboards/Keyboard.js
+++ src/components/MyKeyboards/Keyboard.js
@@ -1,52 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-export default class Keyboard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Section>
-        <h2>{this.props.keyboard.title}</h2>
-        <h3>Parts list:</h3>
-        <PartsList>
-          <li>
-            <b>Layout: </b>
-            {this.props.keyboard.layout}
-          </li>
-          <CoverImage src={this.props.keyboard.image} />
-          <li>
-            <b>Keycaps: </b>
-            {this.props.keyboard.keycaps}
-          </li>
-          <li>
-            <b>Switches: </b>
-            {this.props.keyboard.switches}
-          </li>
-          <li>
-            <b>Case: </b>
-            {this.props.keyboard.case}
-          </li>
-          <li>
-            <b>Plate: </b>
-            {this.props.keyboard.plate}
-          </li>
-          <li>
-            <b>Stabilizers: </b>
-            {this.props.keyboard.stabilizers}
-          </li>
-          <li>
-            <b>Other mods: </b>
-            {this.props.keyboard.mods}
-          </li>
-          <li>{this.props.keyboard.description}</li>
-        </PartsList>
-        <Gallery></Gallery>
-      </Section>
-    );
-  }
-}
+const Keyboard = (props) => {
+  return (
+    <Section>
+      <h2>{props.keyboard.title}</h2>
+      <h3>Parts list:</h3>
+      <PartsList>
+        <li>
+          <b>Layout: </b>
+          {props.keyboard.layout}
+        </li>
+        <CoverImage src={props.keyboard.image} />
+        <li>
+          <b>Keycaps: </b>
+          {props.keyboard.keycaps}
+        </li>
+        <li>
+          <b>Switches: </b>
+          {props.keyboard.switches}
+        </li>
+        <li>
+          <b>Case: </b>
+          {props.keyboard.case}
+        </li>
+        <li>
+          <b>Plate: </b>
+          {props.keyboard.plate}
+        </li>
+        <li>
+          <b>Stabilizers: </b>
+          {props.keyboard.stabilizers}
+        </li>
+        <li>
+          <b>Other mods: </b>
+          {props.keyboard.mods}
+        </li>
+        <li>{props.keyboard.description}</li>
+      </PartsList>
+      <Gallery></Gallery>
+    </Section>
+  );
+};
+
+export default Keyboard;
 
 const Section = styled.div``;
 
--- src/components/MyKeyboards/MyKeyboards.js
+++ src/components/MyKeyboards/MyKeyboards.js
@@ -1,23 +1,20 @@
 import React from "react";
 import Keyboard from "./Keyboard";
 
-export default class MyKeyboards extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const keyboards = this.props.keyboards.map((keyboard) => (
-      <Keyboard key={keyboard.id} keyboard={keyboard} />
-    ));
+const MyKeyboards = (props) => {
+  const keyboards = props.keyboards.map((keyboard) => (
+    <Keyboard key={keyboard.id} keyboard={keyboard} />
+  ));
 
-    return (
-      <>
-        <header>
-          <h1>My Keyboards</h1>
-          <p>This is a list of my keyboards that I've purchased or built.</p>
-        </header>
-        {keyboards}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <header>
+        <h1>My Keyboards</h1>
+        <p>This is a list of my keyboards that I've purchased or built.</p>
+      </header>
+      {keyboards}
+    </>
+  );
+};
+
+export default MyKeyboards;

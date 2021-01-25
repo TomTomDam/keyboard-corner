246b2fe0bea25597d16c7c19782695d9d2e3dee2
TomTomDam <thomasdam97@gmail.com>
2021-01-25 17:57:21 +0000

    Added content to PlatesAndPCBs page

--- src/components/KeyboardEncyclopedia/KeyboardEncyclopediaCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-const KeyboardEncyclopediaCard = (props) => {
-  return <div></div>;
-};
-
-export default KeyboardEncyclopediaCard;

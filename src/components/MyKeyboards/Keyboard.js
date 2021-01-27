1fa0019071ae84fee9b3ce29670b4870ba2128cb
TomTomDam <thomasdam97@gmail.com>
2021-01-27 11:43:09 +0000

    Transferred Keyboard data from App to MyKeyboards component. Using hooks instead of using a class

--- src/components/MyKeyboards/Keyboard.js
+++ src/components/MyKeyboards/Keyboard.js
@@ -1,45 +1,47 @@
 import React from "react";
 import styled from "styled-components";
-import {Header} from "../../assets/styles/Layout";
+import { Header } from "../../assets/styles/Layout";
 
 const Keyboard = (props) => {
   return (
     <Container>
-      <Header>
-        <h2>{props.keyboard.title}</h2>
-      </Header>
-      <PartsList>
-        <CoverImage src={props.keyboard.image} />
-        <li>
-          <Part>Layout</Part>
-          {props.keyboard.layout}
-        </li>
-        <li>
-          <Part>Keycaps</Part>
-          {props.keyboard.keycaps}
-        </li>
-        <li>
-          <Part>Switches</Part>
-          {props.keyboard.switches}
-        </li>
-        <li>
-          <Part>Case</Part>
-          {props.keyboard.case}
-        </li>
-        <li>
-          <Part>Plate</Part>
-          {props.keyboard.plate}
-        </li>
-        <li>
-          <Part>Stabilizers</Part>
-          {props.keyboard.stabilizers}
-        </li>
-        <li>
-          <Part>Other mods</Part>
-          {props.keyboard.mods}
-        </li>
-        <li>{props.keyboard.description}</li>
-      </PartsList>
+      <CoverImage src={props.keyboard.image} />
+      <TextContainer>
+        <Header>
+          <Heading>{props.keyboard.title}</Heading>
+        </Header>
+        <PartsList>
+          <li>
+            <Part>Layout</Part>
+            {props.keyboard.layout}
+          </li>
+          <li>
+            <Part>Keycaps</Part>
+            {props.keyboard.keycaps}
+          </li>
+          <li>
+            <Part>Switches</Part>
+            {props.keyboard.switches}
+          </li>
+          <li>
+            <Part>Case</Part>
+            {props.keyboard.case}
+          </li>
+          <li>
+            <Part>Plate</Part>
+            {props.keyboard.plate}
+          </li>
+          <li>
+            <Part>Stabilizers</Part>
+            {props.keyboard.stabilizers}
+          </li>
+          <li>
+            <Part>Other mods</Part>
+            {props.keyboard.mods}
+          </li>
+          <li>{props.keyboard.description}</li>
+        </PartsList>
+      </TextContainer>
     </Container>
   );
 };
@@ -47,8 +49,11 @@ const Keyboard = (props) => {
 export default Keyboard;
 
 const Container = styled.div`
-  flex: 1 0 33%;
-  padding: 1rem;
+  display: flex;
+  flex: 1 0 50%;
+  align-items: center;
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 const CoverImage = styled.img`
@@ -56,11 +61,20 @@ const CoverImage = styled.img`
   height: 500px;
 `;
 
+const TextContainer = styled.div`
+  margin-left: 2rem;
+`;
+
+const Heading = styled.h1`
+  margin-top: 0rem;
+`;
+
 const PartsList = styled.ul`
   list-style-type: none;
 `;
 
 const Part = styled.div`
+  flex: 1 0 50%;
   font-weight: bold;
   text-transform: uppercase;
   font-size: 1.125rem;
--- src/components/MyKeyboards/MyKeyboards.js
+++ src/components/MyKeyboards/MyKeyboards.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Keyboard from "./Keyboard";
 import styled from "styled-components";
 
-const MyKeyboards = (props) => {
-  const Keyboards = props.keyboards.map((keyboard) => (
+const MyKeyboards = () => {
+  const [keyboards, setKeyboards] = useState([
+    {
+      id: 1,
+      image: "src\\assets\\images\\MyKeyboards\\Space65 ePBT Kuro Shiro 1.jpg",
+      title: "Space65 CV - Black Gold",
+      switches: "Massdrop Holy Panda - Krytox 203g0, 105 on springs",
+      plate: "Carbon  fiber",
+      keycaps: "ePBT Kuro Shiro",
+      case: "Black and Gold",
+      mods: "Case foam",
+      layout: "65%",
+      stabilizers: "Durock Clear",
+      description: "",
+    },
+    {
+      id: 2,
+      image: "src\\assets\\images\\MyKeyboards\\FirstCustomKeyboard2.jpg",
+      title: "KBDFans 5 Degree",
+      switches: "Gateron Yellow - Krytox 205g0, 105 on springs",
+      plate: "Aluminium",
+      keycaps: "ePBT 9009",
+      case: "Grey",
+      mods: "Case foam",
+      layout: "60%",
+      stabilizers: "Cherry",
+      description: "",
+    },
+    {
+      id: 3,
+      image:
+        "src\\assets\\images\\MyKeyboards\\primus_keyboard_render_whitecyan.png",
+      title: "Primus",
+      switches:
+        "Original Aspiration - Krytox 205g0, 105 on springs, white Deskey switch films",
+      plate: "Polycarbonate",
+      keycaps: "KAT Mizu",
+      case: "White and Cyan, Stainless steel weight",
+      mods: "N/A",
+      layout: "75%",
+      stabilizers: "c3Equalz Tiffany Blue",
+      description: "",
+    },
+  ]);
+
+  const Keyboards = keyboards.map((keyboard) => (
     <Keyboard key={keyboard.id} keyboard={keyboard} />
   ));
 
@@ -13,9 +57,7 @@ const MyKeyboards = (props) => {
         <h1>My Keyboards</h1>
         <p>This is a list of my keyboards that I've purchased or built.</p>
       </header>
-      <Container>
-        {Keyboards}
-      </Container>
+      <Container>{Keyboards}</Container>
     </>
   );
 };
@@ -24,8 +66,10 @@ export default MyKeyboards;
 
 const Container = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
   justify-content: center;
+  text-align: left;
   max-width: 80%;
   margin: 0 auto;
+  padding-top: 1rem;
 `;

d261f9e2a3958ab9181f9369a6954804f13c878f
TomTomDam <thomasdam97@gmail.com>
2021-02-12 15:16:28 +0000

    Readded SQLite queries into router but without service layer. For some reason userService is not working, will look into it later when refactoring

--- api/services/usersService.js
+++ api/services/usersService.js
@@ -2,106 +2,29 @@ const express = require("express");
 const db = require("../database/sqlite");
 
 function getAll() {
-  let sql = "SELECT * FROM Users";
-
-  db.all(sql, [], (err, rows) => {
-    if (err)
-      return {
-        success: false,
-        error: err,
-      };
-      
-    return {
-      success: true,
-      data: rows
-    }
-  });
-
-  return {
-    success: false
-  };
+  //TODO
 }
 
 function getByID(id) {
-  let sql = "SELECT * FROM Users WHERE id = ?";
-  let params = id;
-  db.run(sql, params, (err, row) => {
-    if (err)
-      return {
-        success: false,
-        data: [],
-        error: err,
-      };
-
-    return {
-      success: true,
-      data: row,
-      msg: "Successfully performed /user/getByID."
-    };
-  });
-
-  return {
-    success: false,
-    data: [],
-    msg: "Could not perform /user/getByID.",
-  };
+  //TODO
 }
 
 function createUser(user) {
-  let errors = [];
-  if (!user.firstName) {
-    errors.push("No firstName was specified.");
-  }
-  if (!user.lastName) {
-    errors.push("No lastName was specified.");
-  }
-  if (!user.firstName) {
-    errors.push("No email was specified.");
-  }
-  if (!user.username) {
-    errors.push("No username was specified.");
-  }
-  if (!user.password) {
-    errors.push("No password was specified.");
-  }
-  if (errors.length) {
-    return {
-      success: false,
-      data: [],
-      msg: errors,
-    };
-  }
-
-  let sql = "INSERT INTO User (firstName, lastName, email, username, password) VALUES (?,?,?,?,?)";
-  let params = user;
-  db.run(sql, params, (err, result) => {
-    if (err)
-      return {
-        success: false,
-        data: [],
-        msg: err,
-      };
-
-    return {
-      success: true,
-      data: result,
-      msg: "Successfully performed /user/createUser."
-    };
-  });
-
-  return {
-    success: false,
-    data: [],
-    msg: "Could not perform /user/createUser.",
-  };
+  //TODO
 }
 
-function updateUser() {
-  return "Hello";
+function updateUser(id) {
+  //TODO
 }
 
-function deleteUser() {
-  return "Hello";
+function deleteUser(id) {
+  //TODO
 }
 
-module.exports = { getAll, getByID, createUser, updateUser, deleteUser };
+module.exports = {
+  getAll,
+  getByID,
+  createUser,
+  updateUser,
+  deleteUser,
+};

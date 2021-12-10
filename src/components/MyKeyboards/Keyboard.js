0012077cd05df2f48b931451fc7ecbb5be8b6003
TomTomDam <thomasdam97@gmail.com>
2021-12-10 21:40:57 +0000

    Added editable state with editable inputs and save/discard buttons

--- src/components/MyKeyboards/Keyboard.js
+++ src/components/MyKeyboards/Keyboard.js
@@ -6,6 +6,7 @@ import axios from "axios";
 const Keyboard = (props) => {
   const keyboardApi = "http://localhost:3000/api/keyboard";
   const [keyboard, setKeyboard] = useState({});
+  const [editable, setEditable] = useState(false);
 
   useEffect(async () => {
     const data = await axios
@@ -17,50 +18,76 @@ const Keyboard = (props) => {
       setKeyboard({});
     } else {
       setKeyboard(data);
-      
     }
   }, []);
 
   return (
-    <Container>
-      <CoverImage src={`/src/assets/images/MyKeyboards/${keyboard.image}`} />
-      <TextContainer>
-        <Header>
-          <Heading>{keyboard.title}</Heading>
-        </Header>
-        <PartsList>
-          <li>
-            <Part>Layout</Part>
-            {keyboard.layout}
-          </li>
-          <li>
-            <Part>Keycaps</Part>
-            {keyboard.keycaps}
-          </li>
-          <li>
-            <Part>Switches</Part>
-            {keyboard.switches}
-          </li>
-          <li>
-            <Part>Case</Part>
-            {keyboard.case}
-          </li>
-          <li>
-            <Part>Plate</Part>
-            {keyboard.plate}
-          </li>
-          <li>
-            <Part>Stabilizers</Part>
-            {keyboard.stabilizers}
-          </li>
-          <li>
-            <Part>Other mods</Part>
-            {keyboard.modifications}
-          </li>
-          <li>{keyboard.description}</li>
-        </PartsList>
-      </TextContainer>
-    </Container>
+    <>
+      {editable && <></>}
+      {!editable && (
+        <ButtonContainer>
+          <Button onClick={() => setEditable(!editable)}>Edit</Button>
+          <Button>Delete</Button>
+        </ButtonContainer>
+      )}
+      <Container>
+        <CoverImage src={`/src/assets/images/MyKeyboards/${keyboard.image}`} />
+        <TextContainer>
+          <Header>
+            <Heading>{keyboard.title}</Heading>
+          </Header>
+          <PartsList>
+            <li>
+              <Part>Layout</Part>
+              {!editable && keyboard.layout}
+              {editable && <EditableInput value={keyboard.layout} />}
+            </li>
+            <li>
+              <Part>Keycaps</Part>
+              {!editable && keyboard.keycaps}
+              {editable && <EditableInput value={keyboard.keycaps} />}
+            </li>
+            <li>
+              <Part>Switches</Part>
+              {!editable && keyboard.switches}
+              {editable && <EditableInput value={keyboard.switches} />}
+            </li>
+            <li>
+              <Part>Case</Part>
+              {!editable && keyboard.case}
+              {editable && <EditableInput value={keyboard.case} />}
+            </li>
+            <li>
+              <Part>Plate</Part>
+              {!editable && keyboard.plate}
+              {editable && <EditableInput value={keyboard.switches} />}
+            </li>
+            <li>
+              <Part>Stabilizers</Part>
+              {!editable && keyboard.stabilizers}
+              {editable && <EditableInput value={keyboard.stabilizers} />}
+            </li>
+            <li>
+              <Part>Other mods</Part>
+              {!editable && keyboard.modifications}
+              {editable && <EditableInput value={keyboard.modifications} />}
+            </li>
+            <li>
+              <Part>Description</Part>
+              {!editable && keyboard.description}
+              {editable && <EditableInput value={keyboard.description} />}
+            </li>
+          </PartsList>
+        </TextContainer>
+      </Container>
+      {!editable && <></>}
+      {editable && (
+        <ButtonContainer>
+          <Button>Save</Button>
+          <Button onClick={() => setEditable(!editable)}>Discard</Button>
+        </ButtonContainer>
+      )}
+    </>
   );
 };
 
@@ -76,8 +103,8 @@ const Container = styled.div`
 `;
 
 const CoverImage = styled.img`
-  width: 600px;
-  height: 500px;
+  width: 700px;
+  height: 600px;
 `;
 
 const TextContainer = styled.div`
@@ -98,4 +125,16 @@ const Part = styled.div`
   text-transform: uppercase;
   font-size: 1.125rem;
   margin-top: 1rem;
+`;
+
+const ButtonContainer = styled.div`
+  text-align: center;
+`;
+
+const Button = styled.button`
+  margin: 0.5rem;
+`;
+
+const EditableInput = styled.input`
+  display: block;
 `;
